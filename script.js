a931ab7f1e1c8d87a31c6f62423ec11fadcac822
AadhilMoola <aakmoola@gmail.com>
2022-07-14 07:00:28 +0200

    Create function that changes background color when mouse is hovered over

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 let header_div = document.querySelector('#header')
 let container_div = document.querySelector('#container')
 
-for(let i=0; i<16; i++){
+
+
+function gridCreate(size){
+for(let i=0; i<size; i++){
     let containerRow = document.createElement('div')
     container_div.appendChild(containerRow)
     containerRow.classList.add('containerRow')
- for(let i=0; i<16; i++){
+ for(let i=0; i<size; i++){
     let grid = document.createElement('div')
     grid.classList.add('grid')
     containerRow.appendChild(grid)
 } 
 
+}
+}
+
+gridCreate(10)
+
+let grid_div = document.querySelectorAll('.grid')
+console.log(grid_div)
+
+for(let i=0; i<grid_div.length; i++){
+    grid_div[i].addEventListener('mouseenter',function(e){
+        grid_div[i].style.cssText = 'background-color:red'
+    })
 }
