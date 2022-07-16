66944e0aa8a28f4d7e07e24a02af1d8aa452d853
AadhilMoola <aakmoola@gmail.com>
2022-07-16 11:25:02 +0200

    Create a working square creator for the grid, use grid to align

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 let header_div = document.querySelector('#header')
-let container_div = document.querySelector('#container')
+let body_div = document.querySelector('#bodyDiv')
+let selectors_div = document.querySelector('#selectors')
+let gridSize_button = document.querySelector('#gridSizeButton')
+let container_div = document.querySelector('#gridContainer')
 
 
 
+
+
+/* This Does not work for creating a grid in the same footprint each time
 function gridCreate(size){
-for(let i=0; i<size; i++){
+ for(let i=0; i<size; i++){
     let containerRow = document.createElement('div')
     container_div.appendChild(containerRow)
     containerRow.classList.add('containerRow')
@@ -15,15 +21,28 @@ for(let i=0; i<size; i++){
 } 
 
 }
+
+} */
+
+
+function gridCreate(size){
+    for(let i=0; i<size*size; i++){
+        let grid = document.createElement('div')
+        grid.classList.add('grid')
+        container_div.appendChild(grid)
+    }
 }
 
-gridCreate(10)
+let gridSize =prompt('What size Grid?') //gridSize_button.addEventListener('click', function(){
+    //return (prompt('What size Grid?'))
+//})
+console.log(gridSize)
+
+gridCreate(gridSize)
 
 let grid_div = document.querySelectorAll('.grid')
-console.log(grid_div)
-
 for(let i=0; i<grid_div.length; i++){
-    grid_div[i].addEventListener('mouseenter',function(e){
+    grid_div[i].addEventListener('mouseenter',function(){
         grid_div[i].style.cssText = 'background-color:red'
     })
 }
