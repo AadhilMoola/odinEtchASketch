8eebecd4b2a4631855b6aa51b10e0580902e3b7a
AadhilMoola <aakmoola@gmail.com>
2022-07-19 06:52:28 +0200

    Made a Rainbow button that colors in random colors

--- script.js
+++ script.js
@@ -2,27 +2,8 @@ let header_div = document.querySelector('#header')
 let body_div = document.querySelector('#bodyDiv')
 let selectors_div = document.querySelector('#selectors')
 let gridSize_button = document.querySelector('#gridSizeButton')
+let rainbow_button = document.querySelector('#rainbowButton')
 let container_div = document.querySelector('#gridContainer')
-
-
-
-
-
-/* This Does not work for creating a grid in the same footprint each time
-function gridCreate(size){
- for(let i=0; i<size; i++){
-    let containerRow = document.createElement('div')
-    container_div.appendChild(containerRow)
-    containerRow.classList.add('containerRow')
- for(let i=0; i<size; i++){
-    let grid = document.createElement('div')
-    grid.classList.add('grid')
-    containerRow.appendChild(grid)
-} 
-
-}
-
-} */
 
 
 function gridCreate(size){
@@ -57,6 +38,20 @@ container_div.style.cssText = `grid-template-columns: repeat(${size}, 1fr);`
         gridCreate(size)
     }
       
+})
+
+
+//rainbow button
+rainbow_button.addEventListener('click', function(){
+    let grid_div = document.querySelectorAll('.grid')
+    for(let i=0; i<grid_div.length; i++){
+        let r = Math.random()*255
+        let g = Math.random()*255
+        let b = Math.random()*255
+
+        grid_div[i].addEventListener('mouseenter',function(){
+            grid_div[i].style.cssText = `background-color:rgb(${r}, ${g}, ${b})`
+        })}
 })
 
 
